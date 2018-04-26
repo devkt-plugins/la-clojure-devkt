c5b91fe14188025f5e6b7e12b3f9c6d384c58d93
ice1000 <ice1000kotlin@foxmail.com>
2018-04-27 00:06:25 +0800

    Add application

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,33 +5,40 @@ val kotlinVersion = "1.2.40"
 group = "org.jetbrains.plugin.devkt.clojure"
 version = "v1.1"
 
-plugins { java }
+plugins {
+	java
+	application
+}
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 java.sourceSets {
-  "main" {
-    java.setSrcDirs(listOf("src"))
-    resources.setSrcDirs(listOf("res"))
-  }
+	"main" {
+		java.setSrcDirs(listOf("src"))
+		resources.setSrcDirs(listOf("res"))
+	}
 
-  "test" {
-    java.setSrcDirs(emptyList<Any>())
-    resources.setSrcDirs(emptyList<Any>())
-  }
+	"test" {
+		java.setSrcDirs(emptyList<Any>())
+		resources.setSrcDirs(emptyList<Any>())
+	}
 }
 
 repositories {
-  mavenCentral()
-  jcenter()
-  maven("https://jitpack.io")
+	mavenCentral()
+	jcenter()
+	maven("https://jitpack.io")
+}
+
+application {
+	mainClassName = "org.ice1000.devkt.Main"
 }
 
 dependencies {
-  compileOnly(kotlin("compiler-embeddable", kotlinVersion))
+	compileOnly(kotlin("compiler-embeddable", kotlinVersion))
 	val version = "fffe21c45d"
 	compileOnly(group = "com.github.ice1000.dev-kt", name = "common", version = version)
 	runtime(group = "com.github.ice1000.dev-kt", name = "swing", version = version)
