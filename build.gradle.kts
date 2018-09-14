f569e14df8d9a2fdbb7788c2c0e495b8e6752945
ice1000 <ice1000kotlin@foxmail.com>
2018-09-14 17:01:24 -0400

    Update build script, add perl script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.*
 
 val kotlinVersion = "1.2.70"
@@ -8,6 +9,10 @@ version = "v1.1"
 plugins {
 	java
 	application
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions { freeCompilerArgs = listOf("-Xjvm-default=enable") }
 }
 
 java {
@@ -30,6 +35,7 @@ java.sourceSets {
 repositories {
 	mavenCentral()
 	jcenter()
+	maven("https://dl.bintray.com/ice1000/ice1000")
 	maven("https://jitpack.io")
 }
 
